1da977e37b1ebbe6963d235e2c00640ab4de0493
Nikhil Singhal <Nikhilsinghal476@gmail.com>
2024-07-22 18:25:37 +0530

    Add readme file

--- Task 1/NumberGame.java
+++ Task 1/NumberGame.java
@@ -1,20 +1,36 @@
 import java.util.Random;
 import java.util.Scanner;
 
-class NumberGame {
+public class NumberGame {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Attempt: " + i + " out of " + 5);
-            System.out.println("Enter any Number between 1 and 100 ");
-            int Number = sc.nextInt();
-            int gen_Number = random.nextInt(100)+1;
-            boolean result = gen_Number == Number;
-            System.out.println("Generated Number " + gen_Number);
-            System.out.println("Entered Number is " + Number);
-            System.out.println("Result: " + (result ? "Your guess is right " : "Try again "));
-            System.out.println();
+        int attempts = 5;
+        boolean check = true;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("You have " + attempts + " attempts to guess the number between " + 1 + " and " + 100 + ".");
+
+        int generatedNumber = random.nextInt(100) + 1;
+
+        for (int attempt = 1; attempt <= attempts; attempt++) {
+            System.out.print("\nEnter your guess: ");
+            int userGuess = sc.nextInt();
+
+            if (userGuess == generatedNumber) {
+                System.out.println("Congratulations! You guessed it right!");
+                check = false;
+                break;
+            } else if (userGuess < generatedNumber) {
+                System.out.println("Try a higher number.");
+            } else {
+                System.out.println("Try a lower number.");
+            }
         }
+
+        if(check){
+            System.out.println("\nThe correct number was: " + generatedNumber);
+        }
+        System.out.println("Thanks for playing!");
     }
 }
---
+++ Task 1/README.md
@@ -0,0 +1,11 @@
+# Number Game
+
+Welcome to Number Game! This is a simple guessing game where you have to guess a randomly generated number within a certain range.
+
+## How to Play
+
+1. The game will provide feedback on whether your guess is too high or too low.
+2. Keep guessing until you find the correct number.
+3. Enjoy the game and have fun!
+
+
